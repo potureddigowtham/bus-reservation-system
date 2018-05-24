970998d3dca71adbbf32484ac304cd9a1b85aa6d
potureddigowtham <potureddigowtham@gmail.com>
2018-05-24 11:07:00 +0530

    new file:   abc.jpg 	new file:   aslogin.php 	new file:   b.jpg.png 	new file:   b.txt 	new file:   book.php 	new file:   bookticket.php

---
+++ aslogin.php
@@ -0,0 +1,9 @@
+<html>
+<frameset rows="20%,*">
+<frame name="a" src="sdisplay.php" frameborder="0">
+<frameset cols="20%,*">
+<frame name="b" src="sdisplay1.php" frameborder="0">
+<frame name="c" src="sdisplay2.php" frameborder="0">
+</frameset>
+</frameset>
+</html>
---
+++ bookticket.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+  <form action="trains.php" method="post">
+    Source:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="text" name="source" value="" required><br><br>
+    Destination:&nbsp;&nbsp;&nbsp;<input type="text" name="dest" value="" required><br><br>
+    Date of journey:<input type="date" name="date" value="" required><br><br>
+    <input type="submit" name="submit" value="Submit">
+  </form>
+</body>
+</html>
